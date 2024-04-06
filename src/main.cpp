8fb6eb5d8cbd18badcb5f163cbcf516e6cfed082
riverknowledge <t.kawashima23@gmail.com>
2024-04-06 21:48:06 +0900

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,4 @@
-#include <CFDConfig.h>
-// #include <Simulation.h>
-#include <mesh.h>
-#include <readmsh.h>
-
-#include <iostream>
-#include <string>
+#include <FemFy/FemFy.hpp>
 
 using namespace std;
 using namespace FemFy;
@@ -12,19 +6,13 @@ using namespace FemFy;
 int main() {
   cout << "-----------Program start--------------" << endl;
   string configFileName = "./config.json";
-  string meshFileName = "./msh/cube-Body.msh";
+  string meshFileName = "./msh/cube-Body_10_30.msh";
   CFDConfig config;
   Mesh mesh;
   config.loadFromConfigFile(configFileName);
   readMSHFile(meshFileName, mesh);
-  int i(0);
-  std::cout << "mesh.elements.size() = " << mesh.elements.size() << std::endl;
-  for (auto& it : mesh.elements) {
-    cout << i++ << endl;
-    it->printInfo();
-  }
-  //   Simulation sim(config, mesh);
-  //   sim.run();
+  Simulation sim(config, std::move(mesh));
+  sim.run();
   cout << "Finish sim" << endl;
   return 0;
 }
